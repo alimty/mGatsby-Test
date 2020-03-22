272cb5898cd70e1aee69d6ef15193b380aae5716
ali Mengütay <alozicyber@gmail.com>
2020-03-22 23:18:48 +0100

    Gatsby starter prject

---
+++ src/components/ali.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+export const Ali = () => {
+  return (
+    <div>
+      <h2>Multiple componnet in one page</h2>
+    </div>
+  )
+}
+
+export const Gobi = () => {
+  return (
+    <div>
+      <h1> Multiple import use brackets </h1>
+      <p> </p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { Gobi, Ali } from "../components/ali"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,9 +9,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1> Examples practice </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Ali />
+    <Gobi />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
